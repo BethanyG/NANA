6615562ef148a892da4fe2b9c4370d70998c0be0
BethanyG <bethanymgarcia@gmail.com>
2015-08-30 15:09:05 -0700

    DB queries wired up and returning data based on tokenized ingredient line. WOO HOO.

--- IngredientAnalyzer.py
+++ IngredientAnalyzer.py
@@ -34,7 +34,7 @@ connect_to_db(app)
 
 current_recipe = RecipeMaker.parse_recipe("http://www.manjulaskitchen.com/2014/04/09/carrot-ginger-soup")
 
-def set_ingredients(current_recipe):
+def set_ingredient_tokens(current_recipe):
     
     for item in current_recipe.ingredients:
             #set 'dumb' quantity by assuming the first item is quanity
@@ -47,44 +47,96 @@ def set_ingredients(current_recipe):
             item.search_term = nltk.tokenize.word_tokenize(item.source_line)[2]
             #print item.search_term 
     
-   
+    return current_recipe
+
+
+
+def query_for_ingredient(Ingredient):
+    
     
     QUERY = '''SELECT
                 	food_descriptions.ndb_no, food_descriptions.long_desc, 
-                   weights.amount, weights.measurement_desc, weights.gram_weight, 
-                   similarity(food_descriptions.long_desc, '{ingredient}') AS sim_score, 
-                   similarity(weights.measurement_desc, '{measurement}') AS sim_score_measure
-        FROM
+                  weights.amount, weights.measurement_desc, weights.gram_weight, 
+                  similarity(food_descriptions.long_desc, '{ingredient}') AS sim_score, 
+                  similarity(weights.measurement_desc, '{measurement}') AS sim_score_measure
+               FROM
         		food_descriptions
-        JOIN
+               JOIN
         		weights ON food_descriptions.ndb_no = weights.ndb_no
-        WHERE
+               WHERE
         		food_descriptions.long_desc % '{ingredient}' 
-        		AND 
+                  AND 
         			similarity(food_descriptions.long_desc, '{ingredient}') > 0.35
-        		AND 
+                  AND 
         			similarity(weights.measurement_desc, '{measurement}') > 0.035;'''
     
     
-    term = "carrots"
-    #print term
-    measure = "cups"
-    #print measure
-    TEST_Q = QUERY.format(ingredient=term, measurement=measure)    
-    print TEST_Q 
-    test = db.engine.execute(text(TEST_Q))
+    if Ingredient.search_term == None:
+        return Ingredient
     
-    for row in test:
-        print row.long_desc
-    #print QUERY
-    #test2 = db.engine.execute(text(TEST.format(ingredient="asparagus", measurement="spear"))) 
-    #test = db.session.query(Food_Descriptions).from_statement(QUERY)
-    
-    
+    else:    
+        term, measure = Ingredient.search_term, Ingredient.measure
+        #print term
+        #print measure
+        INGRED_QUERY = QUERY.format(ingredient=term, measurement=measure)    
+        #print INGRED_QUERY 
+        ingred_query_result = db.engine.execute(text(INGRED_QUERY))
+        
+        first_row = ingred_query_result.fetchone()     #fetches the first row and processes it differently
+        
+        if first_row != None:    
+            Ingredient.ndb_no  = first_row[0]          #setting ndb_no for nutrition query
+            Ingredient.measure = first_row[3]          #setting measure term for nutrition query
+            
+            Ingredient.db_item_match = first_row.ndb_no, first_row.long_desc + " " + str(first_row.amount) + first_row.measurement_desc
+          
+            for row in ingred_query_result:            #goes through the remaining rows in query and adds them to the alternates list      
+                Ingredient.ndb_alternates[(row.measurement_desc)] = [row.ndb_no, row.long_desc + " " + str(row.amount) + row.measurement_desc]
+        else:
+            Ingredient.ndb_no  = None
+            Ingredient.measure = None
+            ingredient.ndb_alternates = None
 
-    #return current_recipe
+    return Ingredient
  
- 
+def query_for_ingredient_nutrition(Ingredient):
+        
+    QUERY = '''SELECT 
+                   nutrient_definitions.nutrient_desc,
+                   round((weights.gram_weight * nutrient_data.nutrient_val / 100), 4) as Value_per_portion,
+                   nutrient_definitions.units,
+                   nutrient_definitions.num_decimals,
+                   weights.measurement_desc,
+                   nutrient_data.num_data_pts
+               FROM 
+                   weights
+               JOIN
+                   nutrient_data
+                   ON weights.ndb_no = nutrient_data.ndb_no
+               JOIN 
+                   nutrient_definitions
+                   ON nutrient_data.nutrient_no = nutrient_definitions.nutrient_no
+               JOIN
+                   user_nutrients
+                   ON nutrient_data.nutrient_no = user_nutrients.nutrient_no and user_nutrients.user_id = 0
+               WHERE
+                   nutrient_data.ndb_no = '{ndb_no}' and weights.measurement_desc = '{measure}'
+                   ORDER BY nutrient_definitions.nutrient_desc ASC;'''
+        
+    
+    if Ingredient.ndb_no == None:
+        return Ingredient
+    
+    else:
+        ndb_no, measure = Ingredient.ndb_no, Ingredient.measure
+        
+        NUT_QUERY = QUERY.format(ndb_no=ndb_no, measure=measure) 
+        nutrition_query_result = db.engine.execute(text(NUT_QUERY)) 
+        
+        for row in nutrition_query_result:
+            Ingredient.nutrition_values[row.nutrient_desc] = [row.value_per_portion, row.units]
+    
+    return Ingredient
  
  
 '''
@@ -106,10 +158,21 @@ for s in sentence:
     item.search_term = str(search_term)
 '''
 
-#return current_recipe
-#def perform_search(current_recipe):
-
 set_ingredients(current_recipe)
+
+for ingredient in current_recipe.ingredients:  
+    ingredient = query_for_ingredient(ingredient)
+
+for ingredient in current_recipe.ingredients:
+    ingredient = query_for_ingredient_nutrition(ingredient)
+
+
+#print(current_recipe.ingredients[1])
+
+print(current_recipe.ingredients)
+
+
+#print(current_recipe.ingredients[1].nutrition_values)
 
 #for item in current_recipe.ingredients:
 #    print (item.__dict__)    
