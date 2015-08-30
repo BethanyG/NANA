470792d354cffb25d6505e64543aa91303b2f087
BethanyG <bethanymgarcia@gmail.com>
2015-08-29 20:26:32 -0700

    Changed code to call new Recipe.make_json to jsonify the recipe for the front end.

--- server.py
+++ server.py
@@ -29,17 +29,22 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/', methods=['GET', 'POST'])
 def start_here():
 
-    recipes = ['http://www.manjulaskitchen.com/2014/04/09/carrot-ginger-soup',
+    recipes = ['http://www.101cookbooks.com/archives/mung-yoga-bowl-recipe.html',
+               'http://www.manjulaskitchen.com/2014/04/09/carrot-ginger-soup',
                'http://www.manjulaskitchen.com/2015/07/14/paneer-bhurji',
                'http://www.manjulaskitchen.com/2015/08/14/eggless-omelet-vegan',
                'http://www.101cookbooks.com/archives/cocagne-bean-artichoke-salad-recipe.html',             
                'http://www.101cookbooks.com/archives/avocado-asparagus-tartine-recipe.html',
-               'http://www.101cookbooks.com/archives/thai-zucchini-soup-recipe.html']
+               'http://www.101cookbooks.com/archives/thai-zucchini-soup-recipe.html',
+               'http://www.101cookbooks.com/archives/a-good-shredded-salad-recipe.html',
+               'http://www.101cookbooks.com/archives/goldencrusted-brussels-sprouts-recipe.html',    
+               'http://www.101cookbooks.com/archives/summer-berry-crisp-recipe.html',
+               'http://www.101cookbooks.com/archives/caramelized-fennel-on-herbed-polenta-recipe.html']
 
     return render_template("homepage_template.html", recipes=recipes)
 
 
-@app.route("/analysis-url", methods=['GET', 'POST'])
+@app.route("/analysis-url", methods=['GET'])
 #Bug:  When POST is used, RecipeMaker.parse_recipe is called twice in a row, and I can't
 #figure out why!!!
 def new_analysis_requested():
@@ -47,7 +52,7 @@ def new_analysis_requested():
     recipe = request.args.get('recipe-urls')
     
     current_recipe = RecipeMaker.parse_recipe(recipe)
-    recipe_details = RecipeMaker.make_recipe_json(current_recipe)
+    recipe_details = Recipe.make_json(current_recipe)
     
     return render_template("analysis_url.html", recipe=recipe, recipe_details=recipe_details)
 
