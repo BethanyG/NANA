7654ea875b8d4c4920b1ee5551a1cb6492f98539
BethanyG <bethanymgarcia@gmail.com>
2015-08-31 11:50:50 -0700

    cleaned up ingredients and summarized across recipe.

--- IngredientAnalyzer.py
+++ IngredientAnalyzer.py
@@ -26,150 +26,178 @@ from pattern.en import pluralize, singularize
 #from pattern.search import search
 #from pattern.search import search, taxonomy
 #from pattern.search import match
+import json
 from RecipeMaker import *
 from server import app
 from model import *
     
 connect_to_db(app)
 
-current_recipe = RecipeMaker.parse_recipe("http://www.manjulaskitchen.com/2014/04/09/carrot-ginger-soup")
+#current_recipe = RecipeMaker.parse_recipe("http://www.manjulaskitchen.com/2014/04/09/carrot-ginger-soup")
 
-def set_ingredient_tokens(current_recipe):
+class IngredientAnalyzer(object):
     
-    for item in current_recipe.ingredients:
-            #set 'dumb' quantity by assuming the first item is quanity
-            item.quantity = nltk.tokenize.word_tokenize(item.source_line)[0]
-            #print item.quantity
-            #set 'dumb measurement unit by assuming the second item is units            
-            item.measure = nltk.tokenize.word_tokenize(item.source_line)[1]
-            #print item.measure
-            #set 'dumb search term by assuming he third item is the ingredient            
-            item.search_term = nltk.tokenize.word_tokenize(item.source_line)[2]
-            #print item.search_term 
-    
-    return current_recipe
-
-
-
-def query_for_ingredient(Ingredient):
-    
-    
-    QUERY = '''SELECT
-                	food_descriptions.ndb_no, food_descriptions.long_desc, 
-                  weights.amount, weights.measurement_desc, weights.gram_weight, 
-                  similarity(food_descriptions.long_desc, '{ingredient}') AS sim_score, 
-                  similarity(weights.measurement_desc, '{measurement}') AS sim_score_measure
-               FROM
-        		food_descriptions
-               JOIN
-        		weights ON food_descriptions.ndb_no = weights.ndb_no
-               WHERE
-        		food_descriptions.long_desc % '{ingredient}' 
-                  AND 
-        			similarity(food_descriptions.long_desc, '{ingredient}') > 0.35
-                  AND 
-        			similarity(weights.measurement_desc, '{measurement}') > 0.035;'''
-    
-    
-    if Ingredient.search_term == None:
-        return Ingredient
-    
-    else:    
-        term, measure = Ingredient.search_term, Ingredient.measure
-        #print term
-        #print measure
-        INGRED_QUERY = QUERY.format(ingredient=term, measurement=measure)    
-        #print INGRED_QUERY 
-        ingred_query_result = db.engine.execute(text(INGRED_QUERY))
+    @staticmethod
+    def set_ingredient_tokens(current_recipe):
         
-        first_row = ingred_query_result.fetchone()     #fetches the first row and processes it differently
+        for item in current_recipe.ingredients:
+                quantity_conversion = {'quarter' : 0.25,'eighth' : 0.125,
+                                        'half' : 0.5,'1/4' : 0.25,
+                                        '1/8' : 0.125,'1/3' : 0.333,
+                                        '2/3' : 0.667,'3/4' : 0.75,
+                                        '1/2' : 0.5,'1' : 1.0,
+                                        '2' : 2.0,'3' : 3.0,
+                                        '4' : 4.0,'5' : 5.0,
+                                        '6' : 6.0,'7' : 7.0,
+                                        '8' : 8.0,'9' : 9.0,
+                                        'a' : 1.0,'few' : 3.0}
+                
+                #set 'dumb' quantity by assuming the first item is quanity
+                prelim_quantity = nltk.tokenize.word_tokenize(item.source_line)[0]
+                print prelim_quantity.strip(" ")                
+                if quantity_conversion[prelim_quantity.strip(" ")] :
+                    prelim_quantity = quantity_conversion[prelim_quantity.strip(" ")]
+                else :
+                    prelim_quantity = 0
+                    
+                item.quantity = prelim_quantity
+                #print item.quantity
+                #set 'dumb measurement unit by assuming the second item is units            
+                item.measure = nltk.tokenize.word_tokenize(item.source_line)[1]
+                #print item.measure
+                #set 'dumb search term by assuming he third item is the ingredient            
+                item.search_term = nltk.tokenize.word_tokenize(item.source_line)[2]
+                #print item.search_term 
         
-        if first_row != None:    
-            Ingredient.ndb_no  = first_row[0]          #setting ndb_no for nutrition query
-            Ingredient.measure = first_row[3]          #setting measure term for nutrition query
+        return current_recipe
+    
+    
+    @staticmethod
+    def query_for_ingredient(Ingredient):
+        
+        
+        QUERY = '''SELECT
+                      food_descriptions.ndb_no, food_descriptions.long_desc, 
+                      weights.amount, weights.measurement_desc, weights.gram_weight, 
+                      similarity(food_descriptions.long_desc, '{ingredient}') AS sim_score, 
+                      similarity(weights.measurement_desc, '{measurement}') AS sim_score_measure
+                   FROM
+            		food_descriptions
+                   JOIN
+            		weights ON food_descriptions.ndb_no = weights.ndb_no
+                   WHERE
+            		food_descriptions.long_desc % '{ingredient}' 
+                      AND 
+            			similarity(food_descriptions.long_desc, '{ingredient}') > 0.35
+                      AND 
+            			similarity(weights.measurement_desc, '{measurement}') > 0.035;'''
+        
+        
+        if Ingredient.search_term == None:
+            return Ingredient
+        
+        else:    
+            term, measure = Ingredient.search_term, Ingredient.measure
+            #print term
+            #print measure
+            INGRED_QUERY = QUERY.format(ingredient=term, measurement=measure)    
+            #print INGRED_QUERY 
+            ingred_query_result = db.engine.execute(text(INGRED_QUERY))
             
-            Ingredient.db_item_match = first_row.ndb_no, first_row.long_desc + " " + str(first_row.amount) + first_row.measurement_desc
-          
-            for row in ingred_query_result:            #goes through the remaining rows in query and adds them to the alternates list      
-                Ingredient.ndb_alternates[(row.measurement_desc)] = [row.ndb_no, row.long_desc + " " + str(row.amount) + row.measurement_desc]
-        else:
-            Ingredient.ndb_no  = None
-            Ingredient.measure = None
-            ingredient.ndb_alternates = None
-
-    return Ingredient
- 
-def query_for_ingredient_nutrition(Ingredient):
-        
-    QUERY = '''SELECT 
-                   nutrient_definitions.nutrient_desc,
-                   round((weights.gram_weight * nutrient_data.nutrient_val / 100), 4) as Value_per_portion,
-                   nutrient_definitions.units,
-                   nutrient_definitions.num_decimals,
-                   weights.measurement_desc,
-                   nutrient_data.num_data_pts
-               FROM 
-                   weights
-               JOIN
-                   nutrient_data
-                   ON weights.ndb_no = nutrient_data.ndb_no
-               JOIN 
-                   nutrient_definitions
-                   ON nutrient_data.nutrient_no = nutrient_definitions.nutrient_no
-               JOIN
-                   user_nutrients
-                   ON nutrient_data.nutrient_no = user_nutrients.nutrient_no and user_nutrients.user_id = 0
-               WHERE
-                   nutrient_data.ndb_no = '{ndb_no}' and weights.measurement_desc = '{measure}'
-                   ORDER BY nutrient_definitions.nutrient_desc ASC;'''
-        
+            first_row = ingred_query_result.fetchone()     #fetches the first row and processes it differently
+            
+            if first_row != None:    
+                Ingredient.ndb_no  = first_row[0]          #setting ndb_no for nutrition query
+                Ingredient.measure = first_row[3]          #setting measure term for nutrition query
+                
+                Ingredient.db_item_match = first_row.ndb_no, first_row.long_desc + " " + str(first_row.amount) + first_row.measurement_desc
+              
+                for row in ingred_query_result:            #goes through the remaining rows in query and adds them to the alternates list      
+                    Ingredient.ndb_alternates[(row.measurement_desc)] = [row.ndb_no, row.long_desc + " " + str(row.amount) + row.measurement_desc]
+            else:
+                Ingredient.ndb_no  = None
+                Ingredient.measure = None
+                Ingredient.ndb_alternates = None
     
-    if Ingredient.ndb_no == None:
         return Ingredient
     
-    else:
-        ndb_no, measure = Ingredient.ndb_no, Ingredient.measure
+    @staticmethod
+    def query_for_ingredient_nutrition(Ingredient):
+            
+        QUERY = '''SELECT 
+                       nutrient_definitions.nutrient_desc,
+                       round((weights.gram_weight * nutrient_data.nutrient_val / 100), 4) as Value_per_portion,
+                       nutrient_definitions.units,
+                       nutrient_definitions.num_decimals,
+                       weights.measurement_desc,
+                       nutrient_data.num_data_pts
+                   FROM 
+                       weights
+                   JOIN
+                       nutrient_data
+                       ON weights.ndb_no = nutrient_data.ndb_no
+                   JOIN 
+                       nutrient_definitions
+                       ON nutrient_data.nutrient_no = nutrient_definitions.nutrient_no
+                   JOIN
+                       user_nutrients
+                       ON nutrient_data.nutrient_no = user_nutrients.nutrient_no and user_nutrients.user_id = 0
+                   WHERE
+                       nutrient_data.ndb_no = '{ndb_no}' and weights.measurement_desc = '{measure}'
+                       ORDER BY nutrient_definitions.nutrient_desc ASC;'''
+            
         
-        NUT_QUERY = QUERY.format(ndb_no=ndb_no, measure=measure) 
-        nutrition_query_result = db.engine.execute(text(NUT_QUERY)) 
+        if Ingredient.ndb_no == None:
+            return Ingredient
         
-        for row in nutrition_query_result:
-            Ingredient.nutrition_values[row.nutrient_desc] = [row.value_per_portion, row.units]
-    
-    return Ingredient
+        else:
+            ndb_no, measure = Ingredient.ndb_no, Ingredient.measure
+            
+            NUT_QUERY = QUERY.format(ndb_no=ndb_no, measure=measure) 
+            nutrition_query_result = db.engine.execute(text(NUT_QUERY)) 
+            
+            for row in nutrition_query_result:
+                Ingredient.nutrition_values[row.nutrient_desc] = [float(row.value_per_portion), row.units]
+        
+        return Ingredient
  
- 
-'''
-***************Someday, I will get this working.  Today is not that day***************  
+    @staticmethod
+    def analysis_summary(ingredients):
+        analysis_summary = {}
+        for item in ingredients:        
+            for entry,value in item.nutrition_values.iteritems():
+                #print value[0]
+                analysis_summary.setdefault(entry, [0])
+                analysis_summary[entry][0] = analysis_summary[entry][0] + value[0] * item.quantity
+                #analysis_summary[entry][1] = value[1]
+            #for entry, value in item.nutrition_values.iteritems():
+            #    analysis_summary[entry][0] = analysis_summary[entry][0] + value[0]  
+        
+        
+        return analysis_summary
 
-#now make parse tree out of ingredient line            
-sentence = parsetree(item.source_line, chunks=True, lemmata=True)
 
-for s in sentence:
-    #filter all the NP (noun phrases) into a chunk list
-    chunk_list = [singularize(chunk.string) for chunk in s.chunks if chunk.type =='NP']
-    print(chunk_list)                
-    #set the best guess on measurement                
-    #item.measure = chunk_list[0]            
-    #set best guess on db search term
-    if len(chunk_list) > 1:
-        search_term = chunk_list[1]
-    else: search_term = chunk_list[0]
-    item.search_term = str(search_term)
-'''
+#current_recipe = RecipeMaker.parse_recipe("http://www.manjulaskitchen.com/2014/04/09/carrot-ginger-soup")
+#IngredientAnalyzer.set_ingredient_tokens(current_recipe)
 
-set_ingredients(current_recipe)
+#for ingredient in current_recipe.ingredients:
+#        ingredient = IngredientAnalyzer.query_for_ingredient(ingredient)
 
-for ingredient in current_recipe.ingredients:  
-    ingredient = query_for_ingredient(ingredient)
+#for ingredient in current_recipe.ingredients:
+#        ingredient = IngredientAnalyzer.query_for_ingredient_nutrition(ingredient)
 
-for ingredient in current_recipe.ingredients:
-    ingredient = query_for_ingredient_nutrition(ingredient)
+
+
+#for ingredient in current_recipe.ingredients:  
+#    ingredient = query_for_ingredient(ingredient)
+
+#for ingredient in current_recipe.ingredients:
+#    ingredient = query_for_ingredient_nutrition(ingredient)
 
 
 #print(current_recipe.ingredients[1])
 
-print(current_recipe.ingredients)
+#print(current_recipe.ingredients)
 
 
 #print(current_recipe.ingredients[1].nutrition_values)
